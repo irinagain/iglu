48eefed73ebe0ad1f090f5087969a61d4c3c88ec
stevebroll <stevebroll@tamu.edu>
2019-06-12 14:14:30 -0500

    add test function (mage) and documentation

---
+++ R/mage.R
@@ -0,0 +1,15 @@
+#' Calculate MAGE (Mean amplitude of glucose excursion)
+#'
+#' @param data DataFrame with column names ("id", "time", and "gl").
+#' @param id Desired value(s) from id column. Default is all id's.
+#' @param sd Standard deviations used to classify "excursions". Default is 1.
+#' @examples
+#' mage(mydata)
+#' mage(mydata, 'Subject 1', 2)
+mage <- function(data, id = unique(data$id), sd = 1){
+  gl_by_id = as.double(data[data$id %in% id,]$gl)
+  abs_diff_mean = abs(gl_by_id - mean(gl_by_id, na.rm = T))
+  mage_out = mean(abs_diff_mean[abs_diff_mean > sd*sd(gl_by_id, na.rm = T)])
+  return(mage_out)
+}
+
