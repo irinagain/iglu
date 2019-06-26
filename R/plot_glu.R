afb358a5607e89343b552f44e725afa5e9f67505
stevebroll <stevebroll@tamu.edu>
2019-06-26 13:41:42 -0500

    change plot axes labels

--- R/plot_glu.R
+++ R/plot_glu.R
@@ -13,7 +13,8 @@ plot_glu <- function(data){
   gl_by_id = as.double(data$gl)
   # ***
   date_by_id = as.POSIXct(data$time)
-  ggplot2::ggplot(data = data, aes(x = date_by_id, y = gl_by_id, group = 1)) +
+  ggplot2::ggplot(data = data, ggplot2::aes(x = date_by_id, y = gl_by_id, group = 1)) +
     ggplot2::geom_line() +
-    ggplot2::scale_x_datetime(name = 'Date')
+    ggplot2::scale_x_datetime(name = 'Date') +
+    ggplot2::scale_y_continuous(name = 'Blood Glucose')
       }
