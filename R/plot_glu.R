0d0d8b67a9de95aa591a91b1b7c92b5daff550a4
stevebroll <stevebroll@tamu.edu>
2019-06-26 13:42:22 -0500

    update documentation with export and (empty) value field

--- R/plot_glu.R
+++ R/plot_glu.R
@@ -16,5 +16,5 @@ plot_glu <- function(data){
   ggplot2::ggplot(data = data, ggplot2::aes(x = date_by_id, y = gl_by_id, group = 1)) +
     ggplot2::geom_line() +
     ggplot2::scale_x_datetime(name = 'Date') +
-    ggplot2::scale_y_continuous(name = 'Blood Glucose')
+    ggplot2::scale_y_continuous(name = 'Blood Glucosed')
       }
