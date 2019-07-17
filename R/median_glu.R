51b3652c22aa39f0974ee589a4a3795bad05b3c3
stevebroll <stevebroll@tamu.edu>
2019-07-17 13:22:18 -0500

    add wrapper for median()

---
+++ R/median_glu.R
@@ -0,0 +1,19 @@
+#' Calculate median glucose level
+#'
+#' @param data DataFrame with column names ("id", "time", and "gl"),
+#' or vector of glucose values as integer, numeric, or double.
+#'
+#' @description Simple wrapper for median().
+#'
+#' @return
+#'
+#' @export
+#'
+#' @examples
+#' mean_glu(data)
+
+median_glu <- function(data){
+  gl_by_id = read_df_or_vec(data)
+  out = median(gl_by_id, na.rm = T)
+  return(out)
+}
