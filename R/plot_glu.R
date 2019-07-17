6de107b21fb278a88510896dba5fde14b05374ad
stevebroll <stevebroll@tamu.edu>
2019-07-17 13:26:55 -0500

    fix bug in plot_glu

--- R/plot_glu.R
+++ R/plot_glu.R
@@ -9,16 +9,20 @@
 #' @examples
 #' plot_glu(data)
 #'
+
 plot_glu <- function(data){
-  output = switch(class(data), 'data.frame' = as.double(data$gl), 'numeric' = as.double(data), 'integer' = as.double(data), 'double' = as.double(data))
-
   gl_by_id = read_df_or_vec(data)
-  switch(is.data.frame(data), TRUE = glu_tsplot(), FALSE = hist(gl_by_id))
+  if(is.data.frame(data)){
+    glu_tsplot <- function(data){
+      date_by_id = as.POSIXct(data$time)
+      ggplot2::ggplot(data = data, ggplot2::aes(x = date_by_id, y = gl_by_id, group = 1)) +
+        ggplot2::geom_line() +
+        ggplot2::scale_x_datetime(name = 'Date') +
+        ggplot2::scale_y_continuous(name = 'Blood Glucose')
+    }
+    glu_tsplot(data)
+  } else{
 
-  glu_tsplot <- function(data){
-  date_by_id = as.POSIXct(data$time)
-  ggplot2::ggplot(data = data, ggplot2::aes(x = date_by_id, y = gl_by_id, group = 1)) +
-    ggplot2::geom_line() +
-    ggplot2::scale_x_datetime(name = 'Date') +
-    ggplot2::scale_y_continuous(name = 'Blood Glucose')
-      }}
+    hist(gl_by_id)
+  }
+}
