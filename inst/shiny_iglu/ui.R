3e97ad795a45d5341352af28002296c47c206938
stevebroll <stevebroll@tamu.edu>
2019-11-15 15:08:25 -0600

    initialize shiny UI with tab for loading Data

--- inst/shiny_iglu/ui.R
+++ inst/shiny_iglu/ui.R
@@ -1,12 +1,18 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(DT)
 
-# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+  titlePanel("Shiny iglu"),
+  tabsetPanel(
+    tabPanel("Data", fluid = TRUE,
+             sidebarLayout(
+               sidebarPanel(fileInput("datafile", "Choose a CSV File",
+                                      multiple =  FALSE,
+                                      accept = ".csv"),
+               ),
+               mainPanel(tableOutput("data"))
+             ))
+  )
+
+))
