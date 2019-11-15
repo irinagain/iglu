95224d0f9a6631eb8a1758f7c4fe55b6170d0dd7
stevebroll <stevebroll@tamu.edu>
2019-11-15 14:58:42 -0600

    initialize inst directory and shiny iglu subdirectory

---
+++ inst/Shiny_iglu/server.R
@@ -0,0 +1,12 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
