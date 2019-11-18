3c6f1f0c03c1f5b1f63a34d0ef83fef22a8668b8
stevebroll <stevebroll@tamu.edu>
2019-11-18 13:48:47 -0600

    add choice options for selecting each parameter

--- inst/shiny_iglu/ui.R
+++ inst/shiny_iglu/ui.R
@@ -15,7 +15,37 @@ shinyUI(fluidPage(
                             textInput('gl', 'Enter column name corresponding to glucose values', value = 'gl')
                ),
                mainPanel(tableOutput("data"))
-             ))
+             )),
+    tabPanel("Metrics", fluid = TRUE,
+             sidebarLayout(
+               sidebarPanel(selectInput('metric', 'Choose Metric', choices = c(`Above Percent` = 'above_percent',
+                                                                               `ADRR` = 'adrr',
+                                                                               `Below Percent` = 'below_percent',
+                                                                               `CONGA` = 'conga',
+                                                                               `CV` = 'cv_glu',
+                                                                               `GRADE` = 'grade',
+                                                                               `GRADE Euglycemia` = 'grade_eugly',
+                                                                               `GRADE Hyperglycemia` = 'grade_hyper',
+                                                                               `GRADE Hypoglycemia` = 'grade_hypo',
+                                                                               `High Blood Glucose Index` = 'hbgi',
+                                                                               `Hyperglycaemia Index` = 'hyper_index',
+                                                                               `Hypoglycaemia Index` = 'hypo_index',
+                                                                               `Index of Glycemic Control` = 'igc',
+                                                                               `In Range Percent` = 'in_range_percent',
+                                                                               `Interquartile Range` = 'iqr_glu',
+                                                                               `J Index` = 'j_index',
+                                                                               `Low Blood Glucose Index` = 'lbgi',
+                                                                               `MAGE` = 'mage',
+                                                                               `Mean` = 'mean_glu',
+                                                                               `Median` = 'median_glu',
+                                                                               `MODD` = 'modd',
+                                                                               `Quantiles` = 'quantile_glu',
+                                                                               `Range` = 'range_glu',
+                                                                               `Standard Deviation` = 'sd_glu',
+                                                                               `Summary Statistics` = 'summary_glu'
+               ))
+               ))
+    )
   )
 
 ))
