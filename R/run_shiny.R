b0a3afcf1a0b371885f3abfb220881a18fe6c4d8
stevebroll <stevebroll@tamu.edu>
2019-12-06 18:15:49 -0600

    fix function to run shiny app from package

--- R/run_shiny.R
+++ R/run_shiny.R
@@ -3,7 +3,7 @@
 
 # https://deanattali.com/2015/04/21/r-package-shiny-app/
 run_shiny <- function() {
-  appDir <- system.file('inst', 'shiny_iglu', package = 'iglu')
+  appDir <- system.file('shiny_iglu', package = 'iglu')
 
-  shiny::runApp(appDir, display.mode = 'external')
+  shiny::runApp(appDir, display.mode = 'normal')
 }
