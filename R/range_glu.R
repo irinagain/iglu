57e0d828e3077d2c1dead6add29c21fd9036d012
stevebroll <stevebroll@tamu.edu>
2019-12-07 14:35:21 -0600

    complete documentation for range_glu

--- R/range_glu.R
+++ R/range_glu.R
@@ -1,16 +1,35 @@
 #' Calculate glucose level range
 #'
-#' @param data DataFrame with column names ("id", "time", and "gl"),
-#' or vector of glucose values as integer, numeric, or double.
+#' @description The function range_glu outputs the distance between minimum
+#' and maximum glucose values per subject. The output is in a data.frame form
+#' by default, with one column and a row corresponding to each subject.
 #'
-#' @description Computes distance between min and max glucose value.
+#' @usage
+#' range_glu(data)
+#'
+#' @param data DataFrame object with column names "id", "time", and "gl",
+#' or numeric vector of glucose values. NA's will be omitted from the glucose
+#' values in calculation of the range.
+#'
+#' @details
+#' A dataframe structure with 1 row for each subject and 1 column
+#' for the range value is returned.
+#'
+#' Wrapping as.numeric() around the range_glu call on a dataset with
+#' a single subject will return a numeric value corresponding to the range.
+#' This will not work for datasets with multiple subjects.
 #'
 #' @return
 #'
 #' @export
 #'
 #' @examples
-#' range_glu(data)
+#' data(example_data_1_subject)
+#' range_glu(example_data_1_subject)
+#'
+#' data(example_data_5_subject)
+#' range_glu(example_data_5_subject)
+#'
 
 range_glu <- function(data){
   range_glu_single = function(data){
