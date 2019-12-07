65f42b432ce87a36baa168c459c382b1964dc173
stevebroll <stevebroll@tamu.edu>
2019-12-06 18:05:28 -0600

    add plotting function for unsorted lasagna plot

--- R/plottingfunctions.R
+++ R/plottingfunctions.R
@@ -8,3 +8,23 @@ tsplot = function(data, hypo, hyper){
     ggplot2::geom_vline(xintercept = hypo, color = 'red') +
     ggplot2::geom_vline(xintercept = hyper, color = 'red')
 }
+
+unsorted_lasagna = function(data, hypo, hyper){
+  subjects = unique(data$id)
+  H.mat = matrix(NA, nrow = length(subjects), ncol = 1000)
+  rownameslist = rep(NA, length(subjects))
+  for(row in 1:length(subjects)){
+    subject_subset = na.omit((data[data$id == subjects[row],]))
+    subject_subset = subject_subset[1:1000,]
+    H.mat[row, 1:1000] = subject_subset$gl
+    rownameslist[row] = paste('S',row, sep = '')
+  }
+  rownames(H.mat) = rownameslist
+
+  colnames(H.mat) = seq(ncol(H.mat))
+
+  lasagnar::lasagna(H.mat, main = 'Unsorted lasagna plot', legend = T,
+                    xlab = 'Measurement ordered by time', ylab = 'Subject')
+}
+
+
