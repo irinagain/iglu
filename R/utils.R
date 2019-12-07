e6b9b7f200d223c1cd2d17f9b50fa9dfaf99b5c4
stevebroll <stevebroll@tamu.edu>
2019-12-06 21:30:35 -0600

    change geom_vline to geom_hline in tsplot

--- R/utils.R
+++ R/utils.R
@@ -79,8 +79,8 @@ tsplot = function(data, hypo, hyper){
     ggplot2::scale_x_datetime(name = 'Date') +
     ggplot2::scale_y_continuous(name = 'Blood Glucose') +
     ggplot2::ggtitle(paste('Time Series plot for', unique(data$id)[1], sep = ' ')) +
-    ggplot2::geom_vline(xintercept = hypo, color = 'red') +
-    ggplot2::geom_vline(xintercept = hyper, color = 'red')
+    ggplot2::geom_hline(yintercept = hypo, color = 'red') +
+    ggplot2::geom_hline(yintercept = hyper, color = 'red')
 }
 
 unsorted_lasagna = function(data){
