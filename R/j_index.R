56ad1390bd9a813e5078e161aa2b5fde1e7fdab8
stevebroll <stevebroll@tamu.edu>
2019-12-07 18:14:57 -0600

    complete documentation for j_index

--- R/j_index.R
+++ R/j_index.R
@@ -1,17 +1,45 @@
 #' Calculate J-index
 #'
-#' @param data DataFrame with column names ("id", "time", and "gl"),
-#' or vector of glucose values as integer, numeric, or double.
+#' @description
+#' The function j_index produces J-index values in data.frame form
+#' with one column and one row per subject.
+#'
+#' @usage
+#' j_index(data)
+#'
+#' @param data DataFrame object with column names "id", "time", and "gl",
+#' or numeric vector of glucose values. NA's will be omitted from the glucose
+#' values in calculation of J-index score.
+#'
 #'
 #' @return
 #'
 #' @export
 #'
-#' @description The J-index is calculated differently for each of the
-#' two measurement units. For mg/dL (unit = 0), J-index = 0.001 * (mean + sd)^2
-#' For mmol/L (unit = 1), J-index = 0.324 * (mean + sd)^2. This method currently is designed for mg/dl.
+#' @details
+#' A dataframe structure with one column and a row for each subject.
+#'
+#' J-index score is calculated by \eqn{.001 * [mean(BG) + sd(BG)]^2}
+#' where BG is the list of Blood Glucose Measurements.
+#'
+#' Wrapping as.numeric() around the j_index call on a dataset with
+#' a single subject will return a numeric value corresponding to the J-index
+#' score. This will not work for datasets with multiple subjects.
+#'
+#' @references
+#' Rodbard (2009) Interpretation of continuous glucose monitoring data:
+#' glycemic variability and quality of glycemic control,
+#' \emph{Diabetes Technology and Therapeutics} \strong{11 Suppl 1},
+#' S55-67. \doi{10.1089/dia.2008.0132}.
+#'
 #' @examples
-#' j_index(data)
+#'
+#' data(example_data_1_subject)
+#' j_index(example_data_1_subject)
+#'
+#' data(example_data_5_subject)
+#' j_index(example_data_5_subject)
+#'
 
 j_index <- function(data){
   j_index_single = function(data){
