0e6ea2e9af80e020d7238e8dec06e0baf1928c19
stevebroll <stevebroll@tamu.edu>
2019-12-10 13:33:56 -0600

    catch metrics that ouput multiple columns and return warning

--- R/metric_scatterplot.R
+++ R/metric_scatterplot.R
@@ -18,9 +18,16 @@ metric_scatter <- function(data, metric1 = 'adrr', metric2 = 'conga',
                            target_ranges = list(c(80,200), c(70,180), c(70,140)),
                            quantiles = c(0, 25, 50, 75, 100),
                            sd_multiplier = 1,
-                           lag = 1,
-
-
+                           lag = 1
                            ){
+  if(metric1 %in% c('above_percent', 'below_percent', 'in_range_percent',
+                    'summary_glu', 'quantile_glu', 'plot_glu')){
+    stop('metric_scatter currently only supports metrics with a single column
+         in their output.')
+  }
 
+
+
+
+}
 }
