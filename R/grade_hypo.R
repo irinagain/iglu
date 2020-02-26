f15c8088056ddae93b6fa7ce3bf0846d0158e626
stevebroll <stevebroll@tamu.edu>
2020-02-26 12:03:26 -0600

    correct function name for single subject caluclation

--- R/grade_hypo.R
+++ R/grade_hypo.R
@@ -47,7 +47,7 @@
 #'
 
 grade_hypo <- function(data, lower = 70){
-  grade_eugly_single = function(data, lower){
+  grade_hypo_single = function(data, lower){
     gl_by_id = na.omit(read_df_or_vec(data))
     grade_vec = 425*(log10(log10(gl_by_id/18))+0.16)^2
     grade_vec[grade_vec > 50] = 50
