b2d816b8b2a8c2345f805b186144939e74248ae6
stevebroll <stevebroll@tamu.edu>
2020-03-25 17:48:01 -0500

    more documentation and styling fixes

--- R/Mvalue.R
+++ R/Mvalue.R
@@ -8,14 +8,18 @@
 #' mvalue(data)
 #'
 #' @inheritParams mean_glu
-#' @param r A reference value corresponding to basal glycemia in normal subjects; default is 90 mg/dL.
+#' @param r A reference value corresponding to basal glycemia in normal
+#'subjects; default is 90 mg/dL.
 #'
 #' @return A data.frame with two columns: subject id and corresponding M value.
 #'
 #' @export
 #'
 #' @references
-#' Schlichtkrull J, Munck O, Jersild M. (1965) The M-value, an index of blood-sugar control in diabetics. \emph{Acta Medica Scandinavica} \strong{177}, 95-102. \doi{https://doi.org/10.1111/j.0954-6820.1965.tb01810.x}.
+#' Schlichtkrull J, Munck O, Jersild M. (1965) The M-value, an index of
+#' blood-sugar control in diabetics. \emph{Acta Medica Scandinavica}
+#' \strong{177}, 95-102.
+#' \doi{https://doi.org/10.1111/j.0954-6820.1965.tb01810.x}.
 #'
 #' @examples
 #' data(example_data_5_subject)
@@ -30,7 +34,9 @@ mvalue <- function(data, r = 90){
   is_vector = attr(data, "is_vector")
   out = data %>%
       dplyr::group_by(id) %>%
-      dplyr::summarise(Mvalue = mean(1000 * abs(log10(gl/r))^3, na.rm = T))
+      dplyr::summarise(
+        Mvalue = mean(1000 * abs(log10(gl / r)) ^ 3, na.rm = T)
+      )
 
   if (is_vector) {
     out$id = NULL
