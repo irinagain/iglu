a2851d0055cea1e149b5727c4d2ca070881f3e60
Irina Gaynanova <irinag@stat.tamu.edu>
2020-04-07 21:56:25 -0500

    Added lasagna plot across days for 1 subject

--- R/lasagna_plots.R
+++ R/lasagna_plots.R
@@ -1,21 +1,20 @@
 #' Lasagna plot of glucose values for 1 subject aligned across times of day
 #'
-#' @inheritParams above_percent
+#' @inheritParams conga
 #' @param plottype String corresponding to plot type, currently supported
 #' options are 'unsorted' for an unsorted single-subject lasagna plot, and 'timesorted' for a
 #' lasgna plot with glucose values sorted for each time point across days.
-#' @param limits
-#' @param midpoint
-#' @param dt0
-#' @param inter_gap
-#' @param tz
+#' @param limits The minimal and maximal glucose values for coloring grid which is gradient from blue (minimal) to red (maximal), see \code{\link[ggplot2]{scale_fill_gradient2}})
+#' @param midpoint The glucose value serving as midpoint (white) of the diverging gradient scale (see \code{\link[ggplot2]{scale_fill_gradient2}}). The default value is 125 mg/dL. The values above are colored in red, and below in blue.
+#' @param dt0 The time frequency for interpolated aligned grid in minutes, the default will match the CGM meter's frequency (e.g. 5 min for Dexcom). @see
+#' @param inter_gap The maximum allowable gap (in minutes) for interpolation of NA glucose values. The values will not be interpolated between the glucose measurements thare are more than inter_gap minutes apart. The default value is 60 min.
 #'
-#' @return
+#' @return A ggplot object corresponding to lasagna plot
 #' @export
 #'
 #' @examples
 #'
-#' plot_lasagna_1subject(example_data_1subject)
+#' plot_lasagna_1subject(example_data_1_subject)
 #'
 plot_lasagna_1subject <- function(data, plottype = c('unsorted', 'timesorted'), limits = c(50, 400), midpoint = 125, dt0 = NULL, inter_gap = 60, tz = ""){
 
@@ -37,12 +36,10 @@ plot_lasagna_1subject <- function(data, plottype = c('unsorted', 'timesorted'), 
 
   title = ""
 
+  plottype = match.arg(plottype)
   if (plottype == 'timesorted'){
     gl_by_id_ip = apply(gl_by_id_ip, 2, sort, decreasing = T, na.last = T)
     title = ", sorted at each time point."
-
-  }else if (plottype != 'unsorted') {
-    warning('Selected plotting type', plottype, 'not supported. Creating unsorted plot by default.')
   }
 
   # Melt the measurements for lasanga plot
