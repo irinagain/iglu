d2a3459ca088d360e15cf9042953675a825ef82a
Irina Gaynanova <irinag@stat.tamu.edu>
2020-04-10 13:17:21 -0500

    m-value equation fix for extra bracket

--- R/m_value.R
+++ R/m_value.R
@@ -15,9 +15,9 @@
 #'
 #' @export
 #'
-#' @details
-#' M-value is computed by averaging the transformed gluvose values, where each
-#' transformed value is equal to \eqn{|{1000 * log_10(glucose/100)|^3}
+#' @details M-value is computed by averaging the transformed gluvose values, where each
+#' transformed value is equal to \eqn{|1000 * log_10(glucose/100)|^3}
+#'
 #' @references
 #' Schlichtkrull J, Munck O, Jersild M. (1965) The M-value, an index of
 #' blood-sugar control in diabetics. \emph{Acta Medica Scandinavica}
