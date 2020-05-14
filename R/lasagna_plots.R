914d98a4c89890f7ab927a650d5fad13445e7b00
stevebroll <stevebroll@tamu.edu>
2020-05-14 13:18:22 -0500

    standardize target value parameter names and default values

--- R/lasagna_plots.R
+++ R/lasagna_plots.R
@@ -8,6 +8,8 @@
 #' @param midpoint The glucose value serving as midpoint (white) of the diverging gradient scale (see \code{\link[ggplot2]{scale_fill_gradient2}}). The default value is 125 mg/dL. The values above are colored in red, and below in blue.
 #' @param dt0 The time frequency for interpolated aligned grid in minutes, the default will match the CGM meter's frequency (e.g. 5 min for Dexcom).
 #' @param inter_gap The maximum allowable gap (in minutes) for interpolation of NA glucose values. The values will not be interpolated between the glucose measurements thare are more than inter_gap minutes apart. The default value is 60 min.
+#' @param LLTR Lower Limit of Target Range, default value is 80 mg/dL.
+#' @param ULTR Upper Limit of Target Range, default value is 140 mg/dL.
 #'
 #' @return A ggplot object corresponding to lasagna plot
 #' @export
@@ -16,7 +18,7 @@
 #'
 #' plot_lasagna_1subject(example_data_1_subject)
 #'
-plot_lasagna_1subject <- function(data, lasagnatype = c('unsorted', 'timesorted'), limits = c(50, 500), midpoint = 105, lower = 70, upper = 140, dt0 = NULL, inter_gap = 60, tz = ""){
+plot_lasagna_1subject <- function(data, lasagnatype = c('unsorted', 'timesorted'), limits = c(50, 500), midpoint = 105, LLTR = 80, ULTR = 140, dt0 = NULL, inter_gap = 60, tz = ""){
 
   id = glucose = day = NULL
   rm(list = c("id", "glucose", "day"))
@@ -49,7 +51,7 @@ plot_lasagna_1subject <- function(data, lasagnatype = c('unsorted', 'timesorted'
   data_l = data.frame(day = rep(data_ip$actual_dates, each = ntimes), hour = rep(time_grid_hours, ndays), glucose = as.vector(t(gl_by_id_ip)))
 
   p = data_l%>%
-    ggplot(aes(x = hour, y = day, fill = glucose)) + geom_tile()  + ylab("Day") + ggtitle(paste0(subject, title, "")) + xlab("Hour") + xlim(c(0, 24)) + scale_fill_gradientn(colors = c("#3182bd", "#deebf7", "white", "#fee0d2", "#de2d26"), na.value = "grey50", values = scales::rescale(c(limits[1], lower, midpoint, upper, limits[2])), limits = limits)
+    ggplot(aes(x = hour, y = day, fill = glucose)) + geom_tile()  + ylab("Day") + ggtitle(paste0(subject, title, "")) + xlab("Hour") + xlim(c(0, 24)) + scale_fill_gradientn(colors = c("#3182bd", "#deebf7", "white", "#fee0d2", "#de2d26"), na.value = "grey50", values = scales::rescale(c(limits[1], LLTR, midpoint, ULTR, limits[2])), limits = limits)
 
   return(p)
 }
@@ -63,6 +65,8 @@ plot_lasagna_1subject <- function(data, lasagnatype = c('unsorted', 'timesorted'
 #' @param lasagnatype String corresponding to plot type when using\code{datatype = "average"}, currently supported options are 'unsorted' for an unsorted lasagna plot, and 'timesorted' for a
 #' lasgna plot with glucose values sorted for each time point across subjects
 #' @param maxd For datatype "all", maximal number of days to be plotted from the study. The default value is 14 days (2 weeks).
+#' @param LLTR Lower Limit of Target Range, default value is 80 mg/dL.
+#' @param ULTR Upper Limit of Target Range, default value is 140 mg/dL.
 #'
 #' @return A ggplot object corresponding to lasagna plot
 #' @export
@@ -74,7 +78,7 @@ plot_lasagna_1subject <- function(data, lasagnatype = c('unsorted', 'timesorted'
 #' plot_lasagna(example_data_5_subject, datatype = "average")
 #' plot_lasagna(example_data_5_subject, datatype = "average", plottype_average = 'timesorted')
 #'
-plot_lasagna <- function(data, datatype = c("all", "average"), lasagnatype = c('unsorted', 'timesorted'), maxd = 14, limits = c(50, 500), midpoint = 105, lower = 70, upper = 140, dt0 = NULL, inter_gap = 60, tz = ""){
+plot_lasagna <- function(data, datatype = c("all", "average"), lasagnatype = c('unsorted', 'timesorted'), maxd = 14, limits = c(50, 500), midpoint = 105, LLTR = 80, ULTR = 140, dt0 = NULL, inter_gap = 60, tz = ""){
 
   lasagnatype = match.arg(lasagnatype)
   datatype = match.arg(datatype)
@@ -115,7 +119,7 @@ plot_lasagna <- function(data, datatype = c("all", "average"), lasagnatype = c('
     data_l = data.frame(subject = rep(subject, each = length(time_grid_hours)), hour = rep(time_grid_hours, ns), glucose = as.vector(t(average24)))
 
     p = data_l%>%
-      ggplot(aes(x = hour, y = subject, fill = glucose)) + geom_tile() + ylab("Subject") + ggtitle(paste0("24 hours averages for all subjects", title, "")) + xlab("Hour") + xlim(c(0, 24)) + scale_fill_gradientn(colors = c("#3182bd", "#deebf7", "white", "#fee0d2", "#de2d26"), na.value = "grey50", values = scales::rescale(c(limits[1], lower, midpoint, upper, limits[2])), limits = limits)
+      ggplot(aes(x = hour, y = subject, fill = glucose)) + geom_tile() + ylab("Subject") + ggtitle(paste0("24 hours averages for all subjects", title, "")) + xlab("Hour") + xlim(c(0, 24)) + scale_fill_gradientn(colors = c("#3182bd", "#deebf7", "white", "#fee0d2", "#de2d26"), na.value = "grey50", values = scales::rescale(c(limits[1], LLTR, midpoint, ULTR, limits[2])), limits = limits)
     return(p)
   }else{
     max_days = max(sapply(gdall, function(x) nrow(x)))
@@ -137,7 +141,7 @@ plot_lasagna <- function(data, datatype = c("all", "average"), lasagnatype = c('
     data_l = data.frame(subject = rep(subject, each = nt * max_days), day = rep(time_grid_days, ns), glucose = as.vector(t(out)))
 
     p = data_l%>%
-      ggplot(aes(x = day + 1, y = subject, fill = glucose)) + geom_tile() + ylab("Subject") + ggtitle("All subjects") + xlab("Day") + geom_vline(xintercept = c(1:max_days)) + scale_x_continuous(breaks = seq(1, max_days, by = 2)) + scale_fill_gradientn(colors = c("#3182bd", "#deebf7", "white", "#fee0d2", "#de2d26"), na.value = "grey50", values = scales::rescale(c(limits[1], lower, midpoint, upper, limits[2])), limits = limits)
+      ggplot(aes(x = day + 1, y = subject, fill = glucose)) + geom_tile() + ylab("Subject") + ggtitle("All subjects") + xlab("Day") + geom_vline(xintercept = c(1:max_days)) + scale_x_continuous(breaks = seq(1, max_days, by = 2)) + scale_fill_gradientn(colors = c("#3182bd", "#deebf7", "white", "#fee0d2", "#de2d26"), na.value = "grey50", values = scales::rescale(c(limits[1], LLTR, midpoint, ULTR, limits[2])), limits = limits)
 
     return(p)
   }
