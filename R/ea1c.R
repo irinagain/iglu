f3f57f15f9c4c9a87e351aab9f905938e4ea4b7b
Marielle Hicban <marbhic@tamu.edu>
2020-07-22 17:04:36 -0500

    finalized ea1c metric

--- R/ea1c.R
+++ R/ea1c.R
@@ -10,25 +10,25 @@
 #' or numeric vector of glucose values.
 #'
 #' @return If a data.frame object is passed, then a tibble object with
-#' two columns: subject id and corresponding GMI is returned. If a vector of glucose
-#' values is passed, then a tibble object with just the GMI value is returned.
+#' two columns: subject id and corresponding eA1C is returned. If a vector of glucose
+#' values is passed, then a tibble object with just the eA1C value is returned.
 #' as.numeric() can be wrapped around the latter to output just a numeric value.
 #'
 #' @export
 #'
 #' @details
 #' A tibble object with 1 row for each subject, a column for subject id and
-#' a column for GMI values is returned. NA glucose values are
-#' omitted from the calculation of the GMI.
+#' a column for eA1C values is returned. NA glucose values are
+#' omitted from the calculation of the eA1C.
 #'
 #' eA1C score is calculated by \eqn{(46.7+mean(BG))/28.7}
 #' where BG is the list of Blood Glucose Measurements.
 #'
 #' @references
-#' Wojcicki (1995) "J"-index. A new proposition of the assessment
-#' of current glucose control in diabetic patients
-#' \emph{Hormone and Metabolic Research} \strong{27} .41-42,
-#' \doi{10.1055/s-2007-979906}.
+#' Nathan (2008) Translating the A1C assay into estimated average glucose
+#' values
+#' \emph{Hormone and Metabolic Research} \strong{31} .1473-1478,
+#' \doi{10.2337/dc08-0545}.
 #'
 #' @examples
 #'
@@ -38,6 +38,8 @@
 #' data(example_data_5_subject)
 #' ea1c(example_data_5_subject)
 #'
+#'@author
+#'Marielle Hicban
 
 ea1c <- function(data){
   gl = id = NULL
