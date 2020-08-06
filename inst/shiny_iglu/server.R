6e6525fbaba3a4cca22578eac0eaa25743622279
David Buchanan <davidb235@tamu.edu>
2020-08-06 18:20:36 -0500

    Add gvp to shiny

--- inst/shiny_iglu/server.R
+++ inst/shiny_iglu/server.R
@@ -24,7 +24,7 @@ shinyServer(function(input, output) {
 
 parameter_type <- reactive({
 
-  if(input$metric %in% c("adrr", "cv_glu", "cv_measures", "grade", "hbgi", "iqr_glu", "j_index", "lbgi", "mad_glu",
+  if(input$metric %in% c("adrr", "cv_glu", "cv_measures", "grade", "gvp", "hbgi", "iqr_glu", "j_index", "lbgi", "mad_glu",
                          "mean_glu", "median_glu", "range_glu", "sd_glu", "sd_measures", "summary_glu")){
     return("none")
   }
