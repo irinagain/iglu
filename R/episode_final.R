8bc29134f90119377c3c8536b39dfd8da26a02b8
Jung Hoon Seo <57701940+moshimor91@users.noreply.github.com>
2021-03-05 15:13:16 -0600

    Modified episode_final.r

--- R/episode_final.R
+++ R/episode_final.R
@@ -28,13 +28,23 @@
 
 episode_calculation <- function(data, hypo_thres=90.0, hyper_thres= 120.0, dur_length = 15){
 
+  ##################### Input Processing  #####################
   data_ip = CGMS2DayByDay(data, dt0 = 5)
 
-  gl_by_id_ip = data_ip[[1]][2,]
+  gl_by_id_ip = data_ip[[1]][1,]
 
   dt0 = data_ip[[3]]
 
   params = list(gl_by_id_ip, hypo_thres, hyper_thres, dur_length, dt0)
+
+  #gl_by_id_ip       : CGM data for a subject
+  #hypo(hyper)_thres : Hypoglycemia and hyperglycemia threshold
+  #dur_length        : Duaration length
+  #dt0               : Default value for an inteveral (default = 5 mins)
+  ##################### Input Ended       #####################
+
+
+  ##################### Plot Function     #####################
 
   plot_episode<- function(params){
     df = data.frame(params[[1]])
@@ -44,107 +54,117 @@ episode_calculation <- function(data, hypo_thres=90.0, hyper_thres= 120.0, dur_l
     print(p2)
   }
 
+  ##################### Plot Function ened#####################
+
+  ##################### Episode computation ###################
   episode<- function(params) {
-    hypo_episode = params[[1]] <= params[[2]]
+
+    if(params[[2]] > params[[3]])
+      warning("WARNING. Hypoglycemia threshold is greater than hyperglycemia threshold. This may affect result")
+
+    gl_by_id_ip = params[[1]][!is.na(params[[1]])]
+
+    hypo_episode = gl_by_id_ip <= params[[2]] # if data is less than or equal to the threshold containing true or false value
+
     hypo_result = hypo_episode[2:length(hypo_episode)] - hypo_episode[1:length(hypo_episode)-1]
 
-    if(params[[1]][length(params[[1]])] <= params[[2]])
-      hypo_result[length(hypo_result)] = -1
-
-    if(params[[1]][1] <= params[[2]] & !is.na(params[[1]][1])) # Handle exception when first point or first sequence is NA
-      hypo_result[1] = 1
-
-    else if (is.na(params[[1]][1])){
-      NonNA <- which(!is.na(params[[1]]))
-      firstNA <- min(NonNA)
-      if(params[[1]][firstNA] < params[[2]]){
-        hypo_result[firstNA] = 1
-      }
-    }
+    hypo_end_point = which(hypo_result %in% -1) + 1
 
     hypo_starting_point = which(hypo_result %in% 1) + 1
-    hypo_end_point = which(hypo_result %in% -1) + 1
+
+    # Handle when hypoglycemia continues until the end
+    if(gl_by_id_ip[length(gl_by_id_ip)] <= params[[2]]){
+      hypo_end_point = c(hypo_end_point, length(hypo_result))
+      hypo_end_point= sort(hypo_end_point)
+    }
+
+    # Handle exception when first point or first sequence is NA
+    if(gl_by_id_ip[1] <= params[[2]] ) {
+      hypo_starting_point = c(hypo_starting_point, 1)
+      hypo_starting_point= sort(hypo_starting_point)
+    }
 
     hypo_dur_length = hypo_end_point - hypo_starting_point
 
-    if(is.null(hypo_dur_length))
-      hypo_dur_length = c(0)
+    if(is.null(hypo_dur_length)) hypo_dur_length = c(0)
 
-    i = 1; hypo_mean = 0
-
-    while(i <= length(hypo_dur_length)){
-      if((hypo_end_point[i] - hypo_starting_point[i]) >= 3 ){
-        interval = seq(hypo_starting_point[i],hypo_end_point[i], 1)
-        interval_val = params[[1]][interval]
-        hypo_mean = hypo_mean + mean(interval_val[!is.na(interval_val)])
-      }
-      i = i + 1
-    }
     hypo_dur_length = hypo_dur_length[hypo_dur_length >= 3]
+
     hypo_duration = mean(hypo_dur_length * dt0)
-    hypo_total <- hypo_result[!is.na(hypo_result) & hypo_result ==1]
 
-    hyper_episode = params[[1]] >= params[[3]]
+    hypo_total = length(hypo_dur_length)
+
+
+    hyper_episode = gl_by_id_ip >= params[[3]]
+
     hyper_result = hyper_episode[2:length(hyper_episode)] - hyper_episode[1:length(hyper_episode)-1]
-
-    if(params[[1]][length(params[[1]])] >= params[[3]]) # Handle exception When data ends but still did not go down to the threshold
-      hyper_result[length(hyper_result)] = -1
-
-    if(params[[1]][1] >= params[[3]] & !is.na(params[[1]][1])) # Handle exception when first point or first sequence is NA
-      hyper_result[1] = 1
-    else if (is.na(params[[1]][1])){
-      NonNA <- which(!is.na(params[[1]]))
-      firstNA <- min(NonNA)
-      if(params[[1]][firstNA] >= params[[3]]){
-        hyper_result[firstNA] = 1
-      }
-    }
-
-    hyper_starting_point = which(hyper_result %in% 1) + 1
 
     hyper_end_point = which(hyper_result %in% -1) + 1
 
+    hyper_starting_point = which(hyper_result %in% 1) + 1
+
+    # Handle when hyperglycemia continues until the end
+    if(gl_by_id_ip[length(gl_by_id_ip)] >= params[[3]]){
+      hyper_end_point = c(hyper_end_point, length(hyper_result))
+      hyper_end_point= sort(hyper_end_point)
+    }
+
+    # Handle exception when first point is greater than hyperglycemia
+    if(gl_by_id_ip[1] >= params[[3]] ) {
+      hyper_starting_point = c(hyper_starting_point, 1)
+      hyper_starting_point= sort(hyper_starting_point)
+    }
+
     hyper_dur_length = hyper_end_point - hyper_starting_point
 
-    #print(hyper_dur_length)
-    if(is.null(hyper_dur_length))
-      hyper_dur_length = c(0)
+    if(is.null(hypo_dur_length)) hypo_dur_length = c(0)
 
-    i = 1; hyper_mean = 0
-
-    while(i <= length(hyper_dur_length)){
-      if((hyper_end_point[i] - hyper_starting_point[i]) >= 3 ){
-        interval = seq(hyper_starting_point[i],hyper_end_point[i], 1)
-        print(interval)
-        interval_val = params[[1]][interval]
-        hyper_mean = hyper_mean + mean(interval_val[!is.na(interval_val)])
-      }
-      i = i + 1
-    }
     hyper_dur_length = hyper_dur_length[hyper_dur_length >= 3]
 
     hyper_duration = mean(hyper_dur_length * dt0)
 
-    hyper_total <- hyper_result[!is.na(hyper_result) & hyper_result ==1]
+    hyper_total = length(hyper_dur_length)
 
-    episodes = c(length(hypo_dur_length), length(hyper_dur_length))
+    episodes = c(hypo_total, hyper_total)
 
     durations = c(hypo_duration, hyper_duration)
+
+    i = 1; hypo_mean = 0
+
+    while(i <= length(hypo_dur_length)){
+      if( (!is.na(hypo_end_point[i]) & !is.na(hypo_starting_point[i])) & (hypo_end_point[i] - hypo_starting_point[i]) >= 3){
+        interval = seq(hypo_starting_point[i],hypo_end_point[i], 1)
+        interval_val = gl_by_id_ip[interval]
+        hypo_mean = hypo_mean + mean(interval_val)
+      }
+      i = i + 1
+    }
+
+    i = 1; hyper_mean = 0
+
+    while(i <= length(hyper_dur_length)){
+      if((!is.na(hyper_end_point[i]) & !is.na(hyper_starting_point[i])) & (hyper_end_point[i] - hyper_starting_point[i]) >= 3 ){
+        interval = seq(hyper_starting_point[i],hyper_end_point[i], 1)
+        interval_val = gl_by_id_ip[interval]
+        hyper_mean = hyper_mean + mean(interval_val)
+      }
+      i = i + 1
+    }
 
     means = c(hypo_mean, hyper_mean)
 
     if (length(hypo_dur_length != 0))
-      Low_Alert = (sum(hypo_dur_length)) / length(params[[1]][!is.na(params[[1]])]) * 100
+      Low_Alert = (sum(hypo_dur_length)) / length(gl_by_id_ip) * 100
     else
       Low_Alert = 0
     if (length(hyper_dur_length != 0))
-      High_Alert = (sum(hyper_dur_length)) / length(params[[1]][!is.na(params[[1]])]) * 100
+      High_Alert = (sum(hyper_dur_length)) / length(gl_by_id_ip) * 100
     else
       High_Alert = 0
 
     Target_Range = 100 - High_Alert - Low_Alert
 
-    Avg_Glucose = mean(params[[1]][!is.na(params[[1]])] )
+    Avg_Glucose = mean(gl_by_id_ip)
 
     range = c(Low_Alert, Target_Range, High_Alert)
 
@@ -154,9 +174,10 @@ episode_calculation <- function(data, hypo_thres=90.0, hyper_thres= 120.0, dur_l
                             "Hypo_mean" = means[1], "Hyper_mean" = means[2],
                             "low_alert" = range[1], "high_alert" = range[3],
                             "target_range" = range[2])
-
     return (dataframe)
   }
+
+  ##################### Episode computation End ###############
 
   plot_episode(params)
 
