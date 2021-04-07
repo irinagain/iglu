50e19e3e209b1fae1e07d7fd667e6d6d14bb4f65
David Buchanan <davidb235@tamu.edu>
2021-04-07 06:18:00 -0500

    Data processers added

---
+++ R/process_data.R
@@ -0,0 +1,154 @@
+#' Data Pre-Processor
+#'
+#' @description A helper function to assist in pre-processing the user-supplied
+#' input data for use with other functions.
+#' Typically, this function will process the data and be stored as another
+#' dataframe. This function ensures that the supplied data complies with every
+#' function within the \code{iglu}   package. See Vignette for further details.
+#'
+#' @usage process_data(data, id = "id", timestamp = "time", glu = "gl", time_parser = as.POSIXct)
+#'
+#' @param data User-supplied dataset containing continuous glucose monitor data. Must
+#' contain data for time and glucose readings at a
+#' minimum.
+#'
+#' @param timestamp Required column name (character string) corresponding to time values in data. The dates can be
+#' in any format parsable by as.POSIXct, or any format accepted by the parser passed to time_parser.
+#'
+#' @param glu Required column name (character string) corresponding to blood glucose values, mg/dl
+#'
+#' @param time_parser Optional function used to convert datetimes to time objects. Defaults to as.POSIXct.
+#'
+#' @return A processed dataframe object that cooperates with every other
+#' function within the iglu package - all column names and formats comply.
+#' @export
+#'
+#' @examples
+#' data("example_data_1_subject")
+#'
+#' # Process example data
+#' processed <- process_data(example_data_1_subject, id = "id", timestamp = "time", glu = "gl", time_parser = as.PoSIXct)
+#'
+#' processed
+#'
+#' data("example_data_5_subject")
+#'
+#' # Process example data
+#' processed_multisubject <- process_data(example_data_5_subject, id = "id", timestamp = "time", glu = "gl", time_parser = as.PoSIXct)
+#'
+#' processed_multisubject
+#'
+
+process_data = function(data,
+                        id = NULL,
+                        timestamp = NULL,
+                        glu = NULL,
+                        time_parser = as.POSIXct) {
+  if (is.data.frame(data) == FALSE) {
+    if (tibble::is_tibble(data) == FALSE) {
+      if (is.vector(data) == FALSE) {
+        stop("Invalid data type, please use dataframe, tibble, or if passing just glucose readings, vector.")
+      }
+    }
+  }
+
+  if (is.data.frame(data) || tibble::is_tibble(data)) {
+    colnames(data) = tolower(colnames(data))
+    if (is.null(id)) {
+      message("No 'id' parameter passed, defaulting id to 1")
+      id = 1
+    }
+    if (is.character(id)) {
+      if (tolower(id) %in% colnames(data) == FALSE) {
+
+        warning("Could not find user-defined id argument name in dataset.\nFor example, if the user defines id=\"Subjects\" but no column is named \"Subjects\"\nThen there will be no matches for \"Subject\"\nCheck spelling of id argument.\nIf the dataset is only for one subject and has no id column, set single_subject = TRUE.")
+
+        if(length(grep(paste("\\bid\\b", sep = ""), names(data))) == 1) {
+
+          stop('Fix user-defined argument name for id. \nNote: A column in the dataset DOES match the name "id": \nIf this is the correct column, indicate as such in function argument. \ni.e. id = "id"\nIf the dataset is only for one subject and has no id column, set single_subject = TRUE.')
+
+        }
+      } else {
+
+        col_idx <- grep(paste("\\b",tolower(id),"\\b", sep = ""), names(data))
+        data <- data[, c(col_idx, (1:ncol(data))[-col_idx])]
+
+        if(colnames(data)[1] != "id"){
+
+          colnames(data)[1] <- "id"
+          data$id <- as.character(data$id)
+        }
+      }
+    } else {
+      stop('User-defined id name must be character.\n')
+    }
+
+    if (is.character(timestamp)) {
+      if (tolower(timestamp) %in% colnames(data) == FALSE) {
+
+        warning("Could not find user-defined timestamp argument name in dataset.\nFor example, if the user defines timestamp=\"time\" but no column is named \"time\"\nThen there will be no matches for \"time\"\nCheck spelling of timestamp argument.")
+
+        if(length(grep(paste("\\btime\\b", sep = ""), names(data))) == 1) {
+
+          stop('Fix user-defined argument name for timestamp. \nNote: A column in the dataset DOES match the name "time": \nIf this is the correct column, indicate as such in function argument. \ni.e. timestamp = "time"')
+
+        }
+      } else {
+
+        col_idx <- grep(paste("\\b",tolower(timestamp),"\\b", sep = ""), names(data))
+        data <- data[, c(1, col_idx, (2:ncol(data))[-col_idx+1])]
+
+        if(colnames(data)[2] != "time"){
+
+          colnames(data)[2] <- "time"
+        }
+
+          tryCatch({
+            data$time <- time_parser(data$time)
+            },error=function(cond) {
+              message("Failed to convert times to POSIXct, ensure times are in convertable format and possible.\nOriginal error message:")
+              message(cond)
+              stop("Error in time conversion.")
+              return(NA)
+            })
+
+        }
+    } else {
+      stop('User-defined timestamp name must be character.\n')
+    }
+
+    if (is.character(glu)) {
+      if (tolower(glu) %in% colnames(data) == FALSE) {
+
+        warning("Could not find user-defined glucose argument name in dataset.\nFor example, if the user defines glu=\"glucose\" but no column is named \"glucose\"\nThen there will be no matches for \"glucose\"\nCheck spelling of glu argument.")
+
+        if(length(grep(paste("\\bgl\\b", sep = ""), names(data))) == 1) {
+
+          stop('Fix user-defined argument name for glucose. \nNote: A column in the dataset DOES match the name "gl": \nIf this is the correct column, indicate as such in function argument. \ni.e. glu = "glucose"')
+
+        }
+      } else {
+
+        col_idx <- grep(paste("\\b",tolower(glu),"\\b", sep = ""), names(data))
+        data <- data[, c(1:2, col_idx, (3:ncol(data))[-col_idx+2])]
+
+        if(colnames(data)[3] != "gl"){
+
+          colnames(data)[3] <- "gl"
+          data$gl <- as.numeric(data$gl)
+        }
+        if (min(data$gl) < 20) {
+          warning("Minimum glucose reading below 20. Data may not be cleaned.")
+        }
+        if (max(data$gl) > 500) {
+          warning("Maximum glucose reading above 500. Data may not be cleaned.")
+        }
+      }
+    } else {
+      stop('User-defined glucose name must be character.\n')
+    }
+
+  }
+  return(data)
+
+}
---
+++ R/read_raw_sensors.r
@@ -0,0 +1,126 @@
+#' Functions to read raw data from a variety of common sensors. Assumes .csv format for all data. Due to limited accessiblity of data,
+#' only importlibre has been tested on actual data as of 4/7/2021.
+#'
+#' Heavily derived from the readers available in the cgmanalysis package's cleandata function.
+#'
+#' @description Several helper functions to assist in reading data directly from sensor outputs. Should return a dataframe in correct format
+#' for use with the rest of the \code{iglu} package. Assumes all data will be readable with base R read.csv function.
+#'
+#' @usage importlibre("cgmdata.csv", id = "read")
+#'
+#' @param filename String matching the name of the data to be read. Assumed to be .csv
+#'
+#' @param id String indicating subject id. Defaults to "read" or "filename" depending on the import function.
+#' A value of "read" will cause the program to attempt to read the subject id from the file. A value of "filename" will cause the
+#' program to use the basename of the filename (i.e. filename without any directory information) with .csv removed as subject id.
+#'
+#' @return A dataframe containing the data read from the provided file.
+#'
+#' @export
+#'
+#' @details A dataframe object with the columns "id", "time" and "gl" and one row per reading will be returned.
+#'
+#' @references
+#' Vigers et al. (2019) cgmanalysis: An R package for descriptive analysis of continuous glucose monitor data
+#' \emph{PLoS ONE} \strong{14(10)}: e0216851,
+#' \doi{10.1371/journal.pone.0216851}
+#'
+#' @examples
+#' importdexcom("johndoe.csv", id = "filename")
+#'
+#' importlibre("yourdata.csv", id = "read")
+#'
+#' importlibrepro("data.csv" id = "read")
+#'
+#' importasc("home/janedoe.csv", id = "filename")
+#'
+#' importipro("cgmsequences.csv", id = "read")
+#'
+
+
+
+importdexcom = function(filename, id= "read") {
+  data = read.csv(filename, stringsAsFactors = FALSE)
+  if (tolower(id) == "read") {
+    id <- data[3,grep("patient",tolower(colnames(data)))]
+  } else if (tolower(id) == "filename") {
+   id = sub("\\..*","",basename(filename))
+  }
+  out = data[,c(grep("timestamp",tolower(colnames(data))),grep("glucose",tolower(colnames(data)))[1])]
+  colnames(out) = c("time", "gl")
+  out$id = id
+
+  out$time <- strptime(out$time, format='%Y-%m-%dT%H:%M:%S', tz = "")
+  out = out[,c(3,1,2)]
+
+  return(out)
+}
+
+importlibre = function(filename, id="read", mmol = F,colnamerow = 2) {
+  data = read.csv(filename, stringsAsFactors = FALSE)
+  if (tolower(id) == "read") {
+    id <- data[1,1]
+  } else if (tolower(id) == "filename") {
+    id = sub("\\..*","",basename(filename))
+  }
+  colnames(data) = data[colnamerow,]
+  data = data[(colnamerow+1):length(data[,1]),]
+  data <- data[,c(grep("timestamp",tolower(colnames(data))),
+                    grep("historic glucose",tolower(colnames(data))))]
+  colnames(data) <- c('time','gl')
+  data$id = id
+  if (mmol) {
+    data$gl = 18*data$gl
+  }
+  return(data)
+}
+
+importlibrepro = function(filename, id="read") {
+  data = read.csv(filename, stringsAsFactors = FALSE)
+  if (tolower(id) == "read") {
+    id <- data[1,1]
+  } else if (tolower(id) == "filename") {
+    id = sub("\\..*","",basename(filename))
+  }
+  colnames(data) <- data[2,]
+  data <- data[-c(1:2),]
+  data <- data[,c("Time","Historic Glucose (mg/dL)")]
+  colnames(data) <- c('time','gl')
+  data$id = id
+  return(data)
+}
+
+importasc = function(filename, id="filename") {
+  data = read.csv(filename, stringsAsFactors = FALSE)
+  if (tolower(id) == "filename") {
+    id = sub("\\..*","",basename(filename))
+  }
+  data$timestamp = paste(data$Date,data$Time)
+  data$sensorglucose = data$Value
+  data = data[,c('time','gl')]
+  data$id = id
+  return(data)
+}
+
+
+importipro = function(filename, id="read") {
+  data = read.csv(filename, stringsAsFactors = FALSE)
+  base::colnames(data) <- data[11,]
+  if (tolower(id) == "read") {
+    id <- data[2,2]
+  } else if (tolower(id) == "filename") {
+    id <- sub("\\..*","",basename(filename))
+  }
+  data <- data[-c(1:11),]
+  if (grepl("- | /",data$Timestamp[1]) == F) {
+    data$Timestamp <- base::as.POSIXct(as.numeric(data$Timestamp)* (60*60*24),
+                                        origin = "1899-12-30",
+                                        tz = "UTC")
+  }
+  data$Timestamp <- base::sub("[.]00","",data$Timestamp)
+  data <- data[,c("time","gl")]
+  base::colnames(data) <- c('time','gl')
+  data$id = id
+  return(data)
+}
+
