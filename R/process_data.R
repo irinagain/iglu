96532b4f99d51a95869c33525763479092e6929e
David Buchanan <davidb235@tamu.edu>
2021-04-11 04:36:27 -0500

    fixes to process_data docs

--- R/process_data.R
+++ R/process_data.R
@@ -34,14 +34,14 @@
 #' data("example_data_1_subject")
 #'
 #' # Process example data
-#' processed <- process_data(example_data_1_subject, id = "id", timestamp = "time", glu = "gl", time_parser = as.PoSIXct)
+#' processed <- process_data(example_data_1_subject, id = "id", timestamp = "time", glu = "gl", time_parser = as.POSIXct)
 #'
 #' processed
 #'
 #' data("example_data_5_subject")
 #'
 #' # Process example data
-#' processed_multisubject <- process_data(example_data_5_subject, id = "id", timestamp = "time", glu = "gl", time_parser = as.PoSIXct)
+#' processed_multisubject <- process_data(example_data_5_subject, id = "id", timestamp = "time", glu = "gl", time_parser = as.POSIXct)
 #'
 #' processed_multisubject
 #'
