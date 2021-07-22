656c7b16015bdcca712e90f4e15fc880369705ea
LChun <lizzie_chun1@tamu.edu>
2021-07-22 17:54:23 -0500

    add example data option to shiny

--- inst/shiny_iglu/ui.R
+++ inst/shiny_iglu/ui.R
@@ -9,24 +9,30 @@ shinyUI(fluidPage(
   tabsetPanel(
     tabPanel("Data", fluid = TRUE,
              sidebarLayout(
-               sidebarPanel(fileInput("datafile", "Choose a CSV File",
-                                      multiple =  FALSE,
-                                      accept = ".csv"),
-                            selectInput("datatype",
-                                        "Choose what format this .csv file is in",
-                                        c("Processed" = "processed",
-                                          "Dexcom" = "Dexcom",
-                                          "FreeStyle Libre" = "FreeStyle Libre",
-                                          "FreeStyle Libre Pro" = "Libre Pro",
-                                          "ASC" = "ASC",
-                                          "iPro" = "iPro"),
-                                        selected = "Processed"),
-                            textInput("subjid", "Enter subject id (for non processed formats, if id in data leave as default)", value = "default"),
-                            textInput('id', 'Enter column name corresponding to subject ID', value = 'id'),
-                            textInput('time', 'Enter column name corresponding to timestamp', value = 'time'),
-                            textInput('gl', 'Enter column name corresponding to glucose values', value = 'gl'),
-                            downloadButton("downloaddata", "Download Data"),
-                            selectInput('tz', 'Select corresponding time zone', choices = c(OlsonNames()))
+               sidebarPanel(selectInput("demodata", label = "Select Dataset",
+                                        choices = c('', `User Datafile` = 'user_data',
+                                                    `Example Data` = 'example_data')),
+                            conditionalPanel(
+                              condition = "input.demodata == 'user_data'",
+                              fileInput("datafile", "Choose a CSV File",
+                                        multiple =  FALSE,
+                                        accept = ".csv"),
+                              selectInput("datatype",
+                                          "Choose what format this .csv file is in",
+                                          c("Processed" = "processed",
+                                            "Dexcom" = "Dexcom",
+                                            "FreeStyle Libre" = "FreeStyle Libre",
+                                            "FreeStyle Libre Pro" = "Libre Pro",
+                                            "ASC" = "ASC",
+                                            "iPro" = "iPro"),
+                                          selected = "Processed"),
+                              textInput("subjid", "Enter subject id (for non processed formats, if id in data leave as default)", value = "default"),
+                              textInput('id', 'Enter column name corresponding to subject ID', value = 'id'),
+                              textInput('time', 'Enter column name corresponding to timestamp', value = 'time'),
+                              textInput('gl', 'Enter column name corresponding to glucose values', value = 'gl'),
+                              downloadButton("downloaddata", "Download Data"),
+                              selectInput('tz', 'Select corresponding time zone', choices = c(OlsonNames()))
+                            ),
                ),
                mainPanel(tableOutput("data"))
              )),
@@ -167,7 +173,7 @@ shinyUI(fluidPage(
                  numericInput(inputId = "lv2hyperThreshold", label = "\nEnter a value for HyperThreshold (level2)",
                               value = 180),
                  numericInput(inputId = "lv1hypoThreshold", label = "\nEnter a value for HypoThreshold (level1)",
-                           value = 100),
+                              value = 100),
                  numericInput(inputId = "lv2hypoThreshold", label = "\nEnter a value for HypoThreshold (level2)",
                               value = 70),
                  radioButtons("colorScheme", "Color Scheme", c("Color Scheme 1", "Color Scheme 2", "Color Scheme 3"))
@@ -181,7 +187,7 @@ shinyUI(fluidPage(
                    column(12, plotOutput("plot_episode_calculation"))
                  )
                )
-            ))
+             ))
   )
 
 
