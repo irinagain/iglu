031312251d530fa794e417c893358dd2e12e973d
L Chun <7ylchun@gmail.com>
2023-11-21 21:34:33 -0600

    re-add meal metrics tests

--- tests/testthat/test-meals.R
+++ tests/testthat/test-meals.R
@@ -5,16 +5,16 @@ out = meal_metrics(test_hall, test_meals, interpolate = TRUE, adjust_mealtimes =
 
 
 test_that("debug", {
-  expect_equal(test_hall[1,3], 118)
+  expect_equal(mean_glu(test_hall)$mean, 127)
 })
 
-# # delta G
-# test_that("no changes on Hall 2133-018 for delta G", {
-#   expect_equal(out$deltag[1], 97.8, tolerance = 0.2)
-#   expect_equal(out$deltag[2], 171, tolerance = 0.2)
-#   expect_equal(out$deltag[3], 77.6, tolerance = 0.2)
-# })
-#
+# delta G
+test_that("no changes on Hall 2133-018 for delta G", {
+  expect_equal(out$deltag[1], 97.8, tolerance = 0.2)
+  expect_equal(out$deltag[2], 171, tolerance = 0.2)
+  expect_equal(out$deltag[3], 77.6, tolerance = 0.2)
+})
+
 # # delta T
 # test_that("no changes on Hall 2133-018 for delta T", {
 #   expect_equal(out$deltat[1], 80, tolerance = 0.2)
