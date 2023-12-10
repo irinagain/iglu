cf4c9c664f35f3157dabe03655be806e0fb2a408
Nathaniel J. Fernandes <njferns020@gmail.com>
2023-12-10 12:47:27 -0600

    updated shiny app to include new parameters for MAGE

--- inst/shiny_iglu/ui.R
+++ inst/shiny_iglu/ui.R
@@ -91,6 +91,10 @@ shinyUI(fluidPage(
                uiOutput("second_parameter_helptext"),
                uiOutput("select_third_parameter"),
                uiOutput("third_parameter_helptext"),
+               uiOutput("select_fourth_parameter"),
+               uiOutput("fourth_parameter_helptext"),
+               uiOutput("select_fifth_parameter"),
+               uiOutput("fifth_parameter_helptext"),
                checkboxInput("filter_sleep_wake", "Calculate metric for sleeping/waking hours?", value = FALSE, width = NULL),
                conditionalPanel(
                  condition = "input.filter_sleep_wake",
