55215728db56dae948309a3cd763b763837b9a3d
Neo Kok <112899144+neokok@users.noreply.github.com>
2024-09-29 17:51:30 -0400

    updated modd test

--- tests/testthat/test-modd.R
+++ tests/testthat/test-modd.R
@@ -15,6 +15,7 @@ test_modd = function(data, lag = 1){
 
 testthat::test_that("modd works", {
   testthat::expect_equal(iglu::modd(test1)$MODD[1], test_modd(test1), tolerance = 0.0001)
+  testthat::expect_equal(iglu::modd(test1, lag = 2)$MODD[1], test_modd(test1, lag = 2), tolerance = 0.0001)
   testthat::expect_equal(iglu::modd(test2)$MODD[1], test_modd(test2), tolerance = 0.0001)
   testthat::expect_equal(out[1], test_modd(test3[test3$id=='Subject 1',]), tolerance = 0.0001)
   testthat::expect_equal(out[2], test_modd(test3[test3$id=='Subject 2',]), tolerance = 0.0001)
