2ccd318a1483db9c1ca3aa62d6e20c1804c470a4
Julian A Wolfson <julianw@umn.edu>
2025-02-26 10:04:46 -0700

    Handle multiple subjects in plot_ranges.R
    Modified the code for plot_ranges() to produce a faceted plot if the data includes multiple subjects

--- R/plot_ranges.R
+++ R/plot_ranges.R
@@ -38,28 +38,36 @@ plot_ranges <- function (data) {
 
   subject = unique(data$id)
   ns = length(subject)
-  if (ns > 1){
-    subject = subject[1]
-    warning(paste("The provided data have", ns, "subjects. The plot will only be created for subject", subject))
-    data = data %>% dplyr::filter(id == subject)
-  }
+  
+ ranges <- agp_metrics(data, shinyformat = FALSE) %>%
+  mutate(very_low = below_54,
+         low = below_70 - below_54,
+         target = in_range_70_180,
+         high = above_180 - above_250,
+         very_high = above_250) %>% 
+  select(id, very_low:very_high) %>% 
+  pivot_longer(c(very_low:very_high),
+               names_to = "range",
+               values_to = "percent")
 
-  ranges <- agp_metrics(data, shinyformat = FALSE) %>%
-    dplyr::select(-c("id", "active_percent", "mean", "GMI", "CV")) %>%
-    dplyr::reframe(range = c("very_low", 'low', 'target', 'high', 'very_high'),
-                     percent = c(below_54, below_70 - below_54, in_range_70_180,
-                                 above_180 - above_250, above_250))
+colors <- c("#F9B500", "#F9F000", "#48BA3C", "#F92D00", "#8E1B1B")
 
-  ranges = ranges %>%
-    dplyr::mutate(range = factor(range, levels = c("very_high", 'high', 'target', 'low', 'very_low')))
+g <- ggplot(data = ranges, 
+            mapping = aes(x = 1, fill = range, y = percent)) + 
+  geom_bar(stat = "identity") + 
+  scale_fill_manual(values = colors, 
+                    drop = FALSE, 
+                    labels = c("Very High (>250 mg/dL)", "High (181-250 mg/dL)", "Target Range (70-180 mg/dL)", "Low (54-69 mg/dL)", "Very Low (<54 mg/dL)")) + 
+  scale_y_continuous(breaks = seq(0, 100, 10), expand = c(0,0)) +
+  scale_x_continuous(expand = c(0,0)) +
+  labs(y = "Percentage") + 
+  theme(axis.ticks.x = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.title.x = element_blank(), 
+        panel.background = element_blank())
 
-  colors <- c("#F9B500", "#F9F000", "#48BA3C", "#F92D00", "#8E1B1B")
-  ggplot(data = ranges, mapping = aes(x = 1, fill = range, y = percent)) +
-    geom_bar(stat = "identity") +
-    scale_fill_manual(values = colors, drop = FALSE, labels = c("Very High (>250 mg/dL)", "High (181-250 mg/dL)", "Target Range (70-180 mg/dL)", "Low (54-69 mg/dL)", "Very Low (<54 mg/dL)")) +
-    scale_y_continuous(breaks = seq(0, 100, 10)) +
-    labs(y = "Percentage") +
-    theme(axis.ticks.x = element_blank(), axis.text.x = element_blank(),
-          axis.title.x = element_blank(), panel.background = element_blank())
+if(ns > 1) g <- g + facet_wrap(~id)
+
+return(g)
 
 }
